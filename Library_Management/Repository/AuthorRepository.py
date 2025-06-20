231164ee3c0da479fceb2c3c5b997fd9b64338a4
yogesh patel <yopatel@bestpeers.in>
2025-06-20 18:31:33 +0530

    create service and repository for Book

--- Library_Management/Repository/AuthorRepository.py
+++ Library_Management/Repository/AuthorRepository.py
@@ -1,7 +1,11 @@
 from Library_Management.Repository.base import BaseRepository
-from Library_Management.models import Author
-from Library_Management.Schema.schema import Author_Created
+from Library_Management.models import Author,Book
+from Library_Management.Schema.schema import Author_Created,Book_Created
 
 class AuthorRepository(BaseRepository[Author, Author_Created]):
     def __init__(self):
         super().__init__(Author)
+
+class BookRepository(BaseRepository[Book, Book_Created]):
+    def __init__(self):
+        super().__init__(Book)
---
+++ Library_Management/Service/book_service.py
@@ -0,0 +1,31 @@
+# services/book_service.py
+
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from Library_Management.Repository.AuthorRepository import BookRepository
+from Library_Management.Schema.schema import Book_Created
+
+class BookService:
+    def __init__(self):
+        self.repo = BookRepository()
+
+    async def create_book(self, db: AsyncSession, data: Book_Created):
+        return await self.repo.create(db, data)
+
+    async def get_books(self, db: AsyncSession):
+        books = await self.repo.get_all(db)
+        if not books:
+            raise HTTPException(status_code=404, detail="No Books found")
+        return books
+
+    async def delete_book(self, db: AsyncSession, id: str):
+        book = await self.repo.get_by_id(db, id)
+        if not book:
+            raise HTTPException(status_code=404, detail="Book not found")
+        await self.repo.delete(db, book)
+
+    async def update_book(self, db: AsyncSession, id: str, data: Book_Created):
+        book = await self.repo.get_by_id(db, id)
+        if not book:
+            raise HTTPException(status_code=404, detail="Book not found")
+        return await self.repo.update(db, book, data)
